2a186436dcdb5601810e3954cc231f3fe3f2e925
RachitaSamant <rachita.samant@gmail.com>
2024-07-14 14:22:04 +0530

    updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,19 @@ class App extends Component<{}, IState> {
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    
+    let x = 0;
+    const interval = setInterval (() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({
+          data: serverResponds,
+          showGraph: true,
+        });
+      });
+      x++;
+      if (x > 1000){
+        clearInterval(interval);
+      }
+    }, 100);
   }
 
   /**
